2f6b5cf723aa1cf75de09cac797488554fe40702
KOOSANGYOON <nevergiveup936@naver.com>
2018-08-09 17:03:04 +0900

    setting: [master] - Do settings before development.

---
+++ src/test/java/ToDoTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ToDoTest {
+    private static final Logger log = LoggerFactory.getLogger(ToDoTest.class);
+
+    @Test
+    public void test() {
+
+    }
+}
